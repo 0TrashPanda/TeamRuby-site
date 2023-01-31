bbb1bf891d7934fe632d55f62e74ed06941223fd
0TrashPanda <jonah.jvdb@gmail.com>
2023-01-31 19:17:17 +0100

    some shit

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,15 @@ button.addEventListener("click", function() {
   navbar.classList.toggle("collapse");
 });
 
+const expandButton = document.querySelector("#expandButton");
+
+// expandButton.addEventListener("click", function () {
+//   this.classList.toggle("expanded");
+// });
+
+expandButton.onfocus = function () {
+  this.classList.edd("expanded");
+}
+expandButton.onfocusout = function () {
+  this.classList.remove("expanded");
+}
