fef84ab93b8cee37b7b7373c0891c4e3d386b084
0trashpanda <jonah.jvdb@gmail.com>
2023-02-06 08:50:06 +0100

    nav highlight

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,26 @@ const navbar_full = document.getElementById("navbar_full");
 navbarHeight_full = navbar_full.clientHeight
 navbar_full.style.marginBottom = -navbarHeight_full + "px";
 
+
+// nav bar highlight
+const navLinks = document.querySelectorAll(".nav-item a");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", function() {
+  const currentPosition = window.pageYOffset;
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 50;
+    const sectionBottom = section.offsetTop + section.offsetHeight - 50;
+
+    if (sectionTop <= currentPosition && currentPosition <= sectionBottom) {
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.hash === `#${section.id}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
