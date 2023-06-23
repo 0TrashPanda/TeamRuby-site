50326f281951389f9ef73c79aca12fcb63aa0c2b
0TrashPanda <jonah.jvdb@gmail.com>
2023-06-23 07:38:35 +0200

    doc for the board

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   mode: 'production',
   content: ["./src/**/*.{html,js}","./projects/**/*.{html,js}"],
   theme: {
     colors: {
+      ...colors,
       white: '#ffffff',
       black: '#000000',
       ruby: '#e0115f',
@@ -12,6 +14,7 @@ module.exports = {
       bg: '#1D1F23',
     },
     backgroundColor: {
+      ...colors,
       transparent: '#00000000',
       black: '#000000',
       primary: '#1D1F23'
@@ -24,5 +27,7 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
